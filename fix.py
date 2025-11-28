7e6bce3b53d4555e151f74a832086bb0e80ef047
Haya <145580738+H4ya@users.noreply.github.com>
2025-11-28 18:21:29 +0300

    Enhance data formatting and save cleaned dataset
    Added data processing steps to format columns and save the cleaned dataset.

--- fix.py
+++ fix.py
@@ -1,7 +1,19 @@
+"""اضفت كل ذا عشان نرتب ونسهل التيبل علينا """
+
 import pandas as pd
-
-dataFile = pd.read_csv('cleaned_data.csv') #read the dataset and store it
-
+path = 'cleaneDataset.csv'
+dataFile = pd.read_csv(path)
+dataFile.insert(0, 'id', [str(i) for i in range(1, len(dataFile) + 1)])
 dataFile['Price'] = dataFile['Price'].astype(float).round(2)
+dataFile['Date of Purchase'] = dataFile['Date of Purchase'].astype(str).str.zfill(8)
+dataFile['Time of Purchase'] = dataFile['Time of Purchase'].astype(str).str.zfill(6)
+dataFile['Date of Journey'] = dataFile['Date of Journey'].astype(str).str.zfill(8)
+dataFile['Time of Purchase'] = dataFile['Time of Purchase'].astype(str).str.zfill(6)
+dataFile['Departure Time'] = dataFile['Departure Time'].astype(str).str.zfill(6)
+dataFile['Arrival Time'] = dataFile['Arrival Time'].astype(str).str.zfill(6)
+dataFile['Actual Arrival Time'] = dataFile['Actual Arrival Time'].astype(str).str.zfill(6)
 
-dataFile.to_csv('dataset_with_float_prices.csv', index=False)
+dataFile.to_csv('cleaneDataset.csv', index=False)
+
+
+
